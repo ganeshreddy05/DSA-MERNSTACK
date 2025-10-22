b05676fc56295f508fb4f229c98989e57cf87b56
ganesh <ganeshreddyg99@example.com>
2025-10-22 22:03:22 +0530

    Remove kanban-Board and add NEWDSA folder

---
+++ classwork/reactjs/todolist/src/App.jsx
@@ -0,0 +1,13 @@
+import Title from  "./Title.jsx"
+ function App(){
+  console.log("hello app component");
+  return(
+    <>
+    <Title/>
+    <Title/>
+    <Title/>
+    <Title/>
+    </>
+  )
+ }
+ export default  App;
---
+++ classwork/reactjs/todolist/src/Title.jsx
@@ -0,0 +1,7 @@
+function Title(){
+ console.log("ganesh redddy")
+    return(
+        <h1>ganesh redddy</h1>
+    )
+}
+export default Title;
